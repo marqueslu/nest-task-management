c86afd76b6d10a9d8cf5791c3955c7208ce3d22e
Lucas Marques <lucas.msilva09@gmail.com>
2020-04-14 23:16:01 -0300

    Creating the database configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
+import { typeOrmConfig } from './config/typeorm.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TasksModule],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), TasksModule],
   controllers: [],
   providers: [],
 })
